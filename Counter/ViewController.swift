1fb26d880d8b13a4866aebc3ae23e9cb7251436e
Squid87 <Squid87@list.ru>
2023-03-18 16:20:56 +0700

    Add new func

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+let now = Date()
+let flags = NSCalendar.Unit(rawValue: UInt.max)
+let components = (Calendar.current as NSCalendar).components(flags, from: now)
 
 class ViewController: UIViewController {
 
+    //переменная для хранения роезультата
+    var increae: Int = 0
+    
+    //Вывод результата
+    @IBOutlet weak var resultLable: UILabel!
+    
+    //Вьющка для хранения истории
+    @IBOutlet weak var historytextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLable.text = "Значение счетчика: \(increae)"
+        historytextView.text = "История изменений: \n"
+        
     }
-
-
+    
+    //Функция увеличения счетчика
+    @IBAction func increaseBitton(_ sender: Any) {
+        increae = increae+1
+        resultLable.text = "Значение счетчика: \(increae)"
+        
+        //«[дата и время]: значение увеличено»
+        historytextView.text.append("[\(components.day!).\(components.month!).\(components.year!)], \(components.hour!):\(components.minute!)], значение изменено на +1 \n")
+    }
+    
+    
+    //Функция уменьшение счетсика
+    @IBAction func decreaseButton(_ sender: Any) {
+        if increae != 0 {
+            increae = increae-1
+            resultLable.text = "Значение счетчика: \(increae)"
+            //«[дата и время]: значение уменьшено»
+            historytextView.text.append("[\(components.day!).\(components.month!).\(components.year!)], \(components.hour!):\(components.minute!)], значение изменено на -1 \n")
+        }
+        //«[дата и время]: попытка уменьшить значение счётчика ниже 0»
+        else{
+            historytextView.text.append("[\(components.day!).\(components.month!).\(components.year!)], \(components.hour!):\(components.minute!)],попытка уменьшить значение счётчика ниже 0 \n")
+        }
+        
+    }
+    
+    //Функция обнуление счетчика
+    @IBAction func resetCounter(_ sender: Any) {
+        
+        increae = 0
+        resultLable.text = "Значение счетчика: \(increae)"
+        //«[дата и время]: значение сброшено»
+        historytextView.text.append("[\(components.day!).\(components.month!).\(components.year!)], \(components.hour!):\(components.minute!)], значение сброшено \n")
+    }
+    
+    
 }
 
